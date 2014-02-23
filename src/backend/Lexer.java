8d0c12fd7f8c7a06cf9f76247d30f30d5e503963
Willy Chen <willy.chen@duke.edu>
2014-02-23 17:36:41 -0500

    Basic design/idea for backend logo interpretation

---
+++ src/backend/Lexer.java
@@ -0,0 +1,5 @@
+package backend;
+//http://en.wikipedia.org/wiki/Lexical_analysis
+public class Lexer {
+
+}
---
+++ src/backend/Parser.java
@@ -0,0 +1,6 @@
+package backend;
+//http://en.wikipedia.org/wiki/Parsing#Parser
+//http://en.wikipedia.org/wiki/Parse_tree
+public class Parser {
+
+}
