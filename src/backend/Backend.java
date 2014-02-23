d65aefc739da040d8ea685531fa1145a32b3cea7
Willy Chen <willy.chen@duke.edu>
2014-02-23 18:52:42 -0500

    Minor work on string parsing and design

---
+++ src/backend/Backend.java
@@ -0,0 +1,23 @@
+package backend;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+//Tentative class name. The api to the backend
+public class Backend {
+	private Lexer myLexer;
+	private Parser myParser;
+	private Translator myTranslator;
+	
+	public Backend(){
+		myLexer = new Lexer();
+		myParser = new Parser();
+		myTranslator = new Translator();
+	}
+	
+	public Queue parse(String string){
+		myLexer.parse(string);
+		return new LinkedList<String>(); //May need to make a command class. Will have to figure out api with turtle
+	}
+
+}
--- src/backend/Lexer.java
+++ src/backend/Lexer.java
@@ -2,4 +2,15 @@ package backend;
 //http://en.wikipedia.org/wiki/Lexical_analysis
 public class Lexer {
 
+	public Lexer(){
+		
+	}
+	
+	public String trimWhiteSpace(String string){ //Removes multiple spaces and linebreaks. Make private for refactoring 
+		return string.trim().replaceAll("[\\t\\n\\r]"," ").replaceAll(" +", " ");
+	}
+	
+	protected void parse(String string){
+		String str = trimWhiteSpace(string);
+	}
 }
