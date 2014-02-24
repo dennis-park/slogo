d08578a2393266fd9896e4f925cc624d69a44b12
Willy <willy.chen@duke.edu>
2014-02-24 18:36:22 -0500

    behaviors of commands tentatively implemeneted

--- src/backend/Command.java
+++ src/backend/Command.java
@@ -1,5 +1,29 @@
 package backend;
 
 public class Command {
+	public Command(){
+		
+	}
+	
+	public Command(String str){
+		
+	}
+	
+	public void addArgumentDouble(Double d){
+		
+	}
+	
+	public void addArgumentCommand(Command c){
+		
+	}
+	
+	public double execute(){
+		return 0;
+	}
+	
+	public Command initialize(){
+		return new Command();
+	}
+	
 
 }
