ea5ddccf242920cae2db0a6c51609cd49ff69104
Dennis Park <dennisjoonpark@gmail.com>
2014-02-25 21:14:33 -0500

    added Turtle basics

--- src/frontend/Turtle.java
+++ src/frontend/Turtle.java
@@ -1,5 +1,39 @@
 package frontend;
 
 public class Turtle {
+	// turtle id --> private int myId;
+	private double myX, myY;
+	private double myAngle;
+
+	private static double DEFAULT_ANGLE = 90.0;
+
+	public Turtle(double x, double y) {
+		this(x, y, DEFAULT_ANGLE);
+	}
+	public Turtle(double x, double y, double angle) {
+		myX = x;
+		myY = y;
+		myAngle = angle;
+	}
+
+	public double getX() {
+		return myX;
+	}
+
+	public double getY() {
+		return myY;
+	}
+
+	public double getAngle() {
+		return myAngle;
+	}
+
+	public void rotate(double newAngle) {
+		myAngle = newAngle;
+	}
 	
+	public void move(double x, double y) {
+		myX = x;
+		myY = y;
+	}
 }
