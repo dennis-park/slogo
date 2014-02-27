28b5adc8cc8c30fd9b511aec08720e7af839114a
Grace Chen <grace.s.chen@duke.edu>
2014-02-27 18:03:38 -0500

    BuiltInCommands restored
    Not sure where it went during merging ;___;

---
+++ src/backend/command/BuiltInCommands.java
@@ -0,0 +1,24 @@
+package backend.command;
+
+import java.util.HashMap;
+
+import backend.command.math.SumCommand;
+import backend.command.turtle.ForwardCommand;
+
+public class BuiltInCommands {
+	private HashMap<String, Command> commands;
+	public BuiltInCommands(){
+		commands = new HashMap<String, Command>();
+		commands.put("FORWARD", new ForwardCommand());
+		commands.put("FD", new ForwardCommand());
+		commands.put("SUM", new SumCommand());
+	}
+	
+	public boolean hasCommand(String token){
+		return commands.containsKey(token);
+	}
+	
+	public Command getCommand(String token){
+		return commands.get(token);
+	}
+}
