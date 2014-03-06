4cabac7e5f1c8ea588500ed933c24d1fff3f232a
Dennis Park <dennisjoonpark@gmail.com>
2014-03-05 22:32:12 -0500

    added command interface and controller methods, yet to be implemented

--- src/slogo/Controller.java
+++ src/slogo/Controller.java
@@ -1,7 +1,8 @@
 package slogo;
 
-import frontend.Frontend;
 import backend.Backend;
+import frontend.Frontend;
+import frontend.Turtle;
 
 public class Controller {
 	
@@ -17,6 +18,54 @@ public class Controller {
 		myBackend = be;
 		myFrontend = fe;
 	}
+	
+	public void update() {
+		// turtle list
+		// parse and evaluate commands
+	}
+	
+	
+	
+	
+	public Workspace getWorkspace() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public class MoveCommand implements Commandable{
+
+		@Override
+		public double move() throws Exception {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public double execute(int turtleID) throws Exception {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		
+	}
+	public class RotateCommand implements Commandable{
+		
+		@Override
+		public double move() throws Exception {
+			return 0;
+		}
+
+		@Override
+		public double execute(int turtleID) throws Exception {
+			Workspace workspace = getWorkspace();
+			workspace.getTurtle(turtleID);
+			return 0;
+		}
+		
+		
+		
+		
+	}
+	
 	
 	// backend to frontend
 	// move
