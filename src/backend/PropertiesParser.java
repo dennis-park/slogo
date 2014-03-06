1882048d0503ac0270b8516644cb7b27a14d1de9
Grace Chen <grace.s.chen@duke.edu>
2014-03-06 12:00:19 -0500

    languages work, comments still need to be hardcoded into command map

--- src/backend/PropertiesParser.java
+++ src/backend/PropertiesParser.java
@@ -12,6 +12,8 @@ public class PropertiesParser {
 	 * Takes in a language of choice and an empty commands map, and populates the map
 	 * with mappings of string commands in a certain language to the strings of the
 	 * respective command classes.
+	 * As of 3/6/14, the comments tag is hardcoded into the commands map due to difficulties
+	 * in getting properties files to recognize hash tags as keys.
 	 * @param language, the String indicating which language should be used
 	 * @param commands, the empty map of commands to class paths
 	 * @throws IOException 
@@ -28,6 +30,7 @@ public class PropertiesParser {
 			String classPath = properties.getProperty(command).toString();
 			commands.put(command.toUpperCase(), classPath);
 		}
+//		commands.put("#", "class path for comments");
 	}
 	
 }
